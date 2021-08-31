df775065f6657461ba3d0b42dbbf513d3a7d0800
willgardner9 <willgardner9@gmail.com>
2021-08-31 16:11:22 +0100

    Delete next.config.js

--- bestreads/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    supabaseKey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDE2NjcxNywiZXhwIjoxOTQ1NzQyNzE3fQ.aN84lJjs3kwmvghQ7ccNU1YLbuG4q7Nkg-13UEa7lCQ",
-  },
-};
